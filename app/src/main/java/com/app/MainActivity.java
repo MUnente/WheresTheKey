ce9ca3cc10c0811fbb9aba16dfe1823df7d7b99a
Murilo Manente <munente2002@gmail.com>
2022-10-11 13:50:44 -0300

    Feature: Adding first main layout

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -3,6 +3,10 @@ package com.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText txtUserId = (EditText) findViewById(R.id.txtUserId);
+        Button btnEnter = (Button) findViewById(R.id.btnEnter);
+
+        btnEnter.setOnClickListener((View view) -> {
+            Toast.makeText(this, "Hello, World!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
